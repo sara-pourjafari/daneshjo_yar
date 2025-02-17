a9ba908ebb99e8570dd4d26e55b2d25279383e87
mohammad <imousavi41@gmail.com>
2025-02-17 21:26:37 +0330

    Add admin dashboard structure and layout files

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,11 @@
+@include('admin.section.header')
+<!--begin::Wrapper-->
+<div class="app-wrapper">
+	@include('admin.section.nav')
+	<!--begin::Sidebar-->
+	@include('admin.section.sidebar')
+	<!--end::Sidebar-->
+	<!--begin::Main-->
+    @include('admin.section.main')
+		<!--end::Content wrapper-->
+@include('admin.section.footer')
